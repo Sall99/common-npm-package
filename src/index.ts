863f02490e4766f6a925ef33a47b41671181acc2
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2021-12-10 11:40:56 +0000

    additional config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+interface Color {
+   red: number,
+   green: number,
+   blue: number,
+   orange: number
+}
+const color: Color = { red: 10, green: 9, blue: 8, orange: 5 }
+
+console.log(color);
+export default color
